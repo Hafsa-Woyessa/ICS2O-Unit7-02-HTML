518bd5a02920e2b3acf325ab0d5c89da2508de0d
HafsaWoyessa <>
2021-06-08 16:12:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// this connects the "guess" button to the "random" fanction
+document.getElementById('button').addEventListener('click', comparision)
+let userInput = 0
+
+
+function comparision () {
+  // get user input and convert to an integer
+  userInput = document.getElementById('input').value
+  userInput = parseInt(userInput)
+
+  // this checks if userInput is less than 0
+  if(userInput < 0) {
+    document.getElementById('answer').innerHTML = 'Negative'
+  } else {
+    document.getElementById('answer').innerHTML = 'Positive'
+  }
+
+}
